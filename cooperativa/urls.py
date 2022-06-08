44208b1c86c26f6a882fb5bbc2611adf62b2dcfd
Ander Gonzalez <andersongonzalez2001@gmail.com>
2022-06-08 18:07:51 -0500

    feat: add custom user model

--- cooperativa/urls.py
+++ cooperativa/urls.py
@@ -4,5 +4,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('supersecret/', admin.site.urls),
+    path("supersecret/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Real Coop Admin"
+admin.site.site_title = "Real Coop Admin Portal"
+admin.site.index_title = "Welcome to the Real Coop Portal"
