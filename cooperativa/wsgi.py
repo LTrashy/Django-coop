331aa0574973d53c8851e5d669e983951cf133c4
Ander Gonzalez <andersongonzalez2001@gmail.com>
2022-06-08 12:05:40 -0500

    chore: split settings.py

--- cooperativa/wsgi.py
+++ cooperativa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cooperativa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cooperativa.settings.development')
 
 application = get_wsgi_application()
